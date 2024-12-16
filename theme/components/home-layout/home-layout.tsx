1995ef143e931a832434613e87f9fcda3322a806
脆薯饼 <Virgil@nanocat.me>
2024-12-16 12:27:46 +0800

    feat: new style

---
+++ theme/components/home-layout/home-layout.tsx
@@ -0,0 +1,4 @@
+import Theme from 'rspress/theme';
+import './home-layout.css';
+
+export const HomeLayout = () => <Theme.HomeLayout />;
--- theme/index.tsx
+++ theme/index.tsx
@@ -1,10 +1,10 @@
 import Theme from 'rspress/theme';
-//import { HomeLayout } from './components/home-layout';
+import { HomeLayout } from './components/home-layout';
 import { Layout } from './components/layout';
 
 export default {
   ...Theme,
-  //HomeLayout,
+  HomeLayout,
   Layout,
 };
 
